b82e4e0f3e7a819ca4b6e21a303c33af32cc54a3
Daniel Newport <dan@newport.solutions>
2021-10-04 03:10:35 -0400

    add config for www.

--- README.md
+++ README.md
@@ -4,6 +4,10 @@
 Both `terraform` and `ansible-playbook` are required to be on your $PATH to work with this project.
 
 ## Variables
+The following variables are required. It is recommended that they be placed in a `*.auto.tfvars` file in the root of this project so they are automatically applied.
+
 | Variable | Description |
 | -------- | ----------- |
+| admin_email | The admin e-mail address. |
 | linode_api_token | The Linode V4 API Token. |
+| ssh_public_key | The public SSH key used to authenticate. |
--- main.tf
+++ main.tf
@@ -47,3 +47,84 @@ resource "linode_domain_record" "caa_iodef" {
   target      = "mailto:${var.admin_email}"
   ttl_sec     = 28800
 }
+
+#
+resource "linode_instance" "www" {
+  label      = "www"
+  private_ip = false
+  region     = "us-southeast"
+  tags       = [ "app:todo", "app:www" ]
+  type       = "g6-standard-2"
+
+  config {
+    devices {
+      sda {
+        disk_label = "root"
+      }
+    }
+    kernel      = "linode/grub2"
+    label       = "boot_config"
+    root_device = "/dev/sda"
+  }
+
+  disk {
+    authorized_keys = [ var.ssh_public_key ]
+    image           = "linode/arch"
+    label           = "root"
+    size            = 81920
+  }
+}
+
+resource "linode_domain_record" "bare_ipv4" {
+  domain_id   = linode_domain.newport_solutions.id
+  name        = ""
+  record_type = "A"
+  target      = tolist(linode_instance.www.ipv4)[0]
+  ttl_sec     = 300
+}
+
+resource "linode_domain_record" "bare_ipv6" {
+  domain_id   = linode_domain.newport_solutions.id
+  name        = ""
+  record_type = "AAAA"
+  target      = trimsuffix(linode_instance.www.ipv6, "/128")
+  ttl_sec     = 300
+}
+
+resource "linode_domain_record" "www_ipv4" {
+  domain_id   = linode_domain.newport_solutions.id
+  name        = "www"
+  record_type = "A"
+  target      = tolist(linode_instance.www.ipv4)[0]
+  ttl_sec     = 300
+}
+
+resource "linode_domain_record" "www_ipv6" {
+  domain_id   = linode_domain.newport_solutions.id
+  name        = "www"
+  record_type = "AAAA"
+  target      = trimsuffix(linode_instance.www.ipv6, "/128")
+  ttl_sec     = 300
+}
+
+resource "linode_rdns" "www_ipv4" {
+  address = tolist(linode_instance.www.ipv4)[0]
+  rdns    = "www.newport.solutions"
+}
+
+resource "linode_rdns" "www_ipv6" {
+  address = trimsuffix(linode_instance.www.ipv6, "/128")
+  rdns    = "www.newport.solutions"
+}
+
+resource "null_resource" "configure" {
+  depends_on = [
+    linode_instance.www
+  ]
+  triggers = {
+    playbook_sha256 = filebase64sha256("playbooks/site.yml")
+  }
+  provisioner "local-exec" {
+    command     = "ansible-playbook playbooks/site.yml"
+  }
+}
